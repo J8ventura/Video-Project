a714f1d9ef828d4e2e6a790e87bfb1deb610c190
Johnny <j8ventura@gmail.com>
2023-03-28 10:24:03 -0500

    working

--- src/components/InventoryContainer.jsx
+++ src/components/InventoryContainer.jsx
@@ -2,14 +2,17 @@ import InventoryItem from "./InventoryItem";
 
 export default function InventoryContainer({inventory}) {
     return (
+        
         <div className = "inventory-container">
         <h2>Inventory</h2>
+
         <div className = "inventory-list"> 
             {inventory.map((movie, index) => <InventoryItem key={index} movie={movie} />)}
-            <InventoryItem />
+            
         </div>
         </div>
         
         
     );
 };
+
--- src/components/InventoryItem.jsx
+++ src/components/InventoryItem.jsx
@@ -1,16 +1,19 @@
 
 
 export default function InventoryItem({movie}){
-    
-    return(
-        <h2>Item Inventory</h2>
-    )
-    
-    // const { title, imgUrl, copiesAvailable } = movie;
-    // console.log(title, imgUrl, copiesAvailable);
-    //     return (
-    //         <>
-    //         <h2>{title}</h2>
-    //         </>
-    //     );
+    const { title, imgUrl, copiesAvailable } = movie;
+    console.log(title, imgUrl, copiesAvailable);
+
+        return (
+            <div className="inventory-item">
+            <h2>{title}</h2>
+            <img src={imgUrl} />
+            <pre>Copies Available: {copiesAvailable}</pre>
+            <button 
+                disabled={copiesAvailable ==0} 
+                onClick={() => alert(`you checked out ${title}`)}
+                >Check out</button>
+            </div>
+        );
 }
+
